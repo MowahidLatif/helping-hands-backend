5b12a2b52cc3ead6dbd4b04fb5a9ca8888f976f6
mowahid latif <mowahidlatif@hotmail.com>
2025-04-26 13:09:59 -0500

    Update init files for route and services

---
+++ app/routes/__init__.py
@@ -0,0 +1,6 @@
+from app.routes.user_routes import user_bp
+from app.routes.campaign_routes import campaign_bp
+from app.routes.donation_routes import donation_bp
+from app.routes.media_routes import media_bp
+
+__all__ = ["user_bp", "campaign_bp", "donation_bp", "media_bp"]
---
+++ app/services/__init__.py
@@ -0,0 +1,6 @@
+from app.services.user_service import *
+from app.services.campaign_service import *
+from app.services.donation_service import *
+from app.services.media_service import *
+
+__all__ = ["user_service", "campaign_service", "donation_service", "media_service"]
