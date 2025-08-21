9f6722d8d7f61b18c652fc5af7b20cd8c36bb1e4
mowahid latif <mowahidlatif@hotmail.com>
2025-08-20 20:08:21 -0500

    Docker and other updates related to privacy and rendering

--- run.py
+++ run.py
@@ -2,4 +2,13 @@ from app import create_app
 
 app = create_app()
 
-# poetry run flask --app app:create_app --debug run
+# poetry run flask:
+#   --app app:create_app --debug run
+# up:
+# 	docker compose --env-file .env.docker up -d
+# down:
+# 	docker compose down
+# logs:
+# 	docker compose logs -f --tail=100
+# nuke:
+# 	docker compose down -v  # WARNING: deletes DB/Redis/S3 data
