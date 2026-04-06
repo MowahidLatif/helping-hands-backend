e1e154a2fe0c1d2d2212eb92bc1587a0f70f4f33
mowahid latif <mowahidlatif@hotmail.com>
2026-04-05 19:32:18 -0500

    remove testing file

--- test_db.py
+++
@@ -1,8 +0,0 @@
-from app.utils.db import get_db_connection
-
-conn = get_db_connection()
-cur = conn.cursor()
-cur.execute("SELECT version()")
-print(cur.fetchone())
-conn.close()
-print("Connection successful.")
